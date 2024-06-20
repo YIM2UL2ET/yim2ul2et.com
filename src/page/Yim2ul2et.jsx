216fd77fddd05053004b4e0abf6eacd506a4aa95
임승호 <132066506+YIM2UL2ET@users.noreply.github.com>
2024-06-20 16:47:47 +0900

    무한 스크롤 구현

--- src/page/Yim2ul2et.jsx
+++ src/page/Yim2ul2et.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Profile from "../features/Page/Profile";
 import Hello from "../features/Page/Hello";
 import History from "../features/Page/History";
@@ -10,12 +10,30 @@ import '@shinyongjun/react-fullpage/css';
 
 export default function Yim2ul2et() {
 
-    const [activeIndex, setActiveIndex] = useState(0);
+    const [activeIndex, setActiveIndex] = useState(5);
+    const [speed, setSpeed] = useState(0);
+    console.log(activeIndex);
+
+    useEffect(() => {
+        setTimeout(() => { setSpeed(1000); }, 80);
+    }, [])
+
+    useEffect(() => {
+        if (activeIndex === 0) {
+            setTimeout(() => { setActiveIndex(5); setSpeed(0); }, 1000);
+            setTimeout(() => { setSpeed(1000); }, 1080);
+        }
+        if (activeIndex === 6) {
+            setTimeout(() => { setActiveIndex(1); setSpeed(0); }, 1000);
+            setTimeout(() => { setSpeed(1000); }, 1080);
+        }
+    }, [activeIndex])
 
     return (
         <FullpageContainer
             activeIndex={activeIndex}
             setActiveIndex={setActiveIndex}
+            transitionDuration={speed}
         >
             <FullpageSection>
                 <Profile />
@@ -31,6 +49,12 @@ export default function Yim2ul2et() {
             </FullpageSection>
             <FullpageSection>
                 <Contect />
+            </FullpageSection>
+            <FullpageSection>
+                <Profile />
+            </FullpageSection>
+            <FullpageSection>
+                <Hello />
             </FullpageSection>
         </FullpageContainer>
     );
